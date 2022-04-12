0e23eca0638f27bbced9fc2dfa02bca7f3f7e0b0
Rajesh Thokala <rajeshthokala10@gmail.com>
2022-04-12 09:49:35 +0530

    added streams group by

--- src/main/java/com/learnings/core/StreamsTest.java
+++ src/main/java/com/learnings/core/StreamsTest.java
@@ -7,37 +7,49 @@ import java.util.stream.Collectors;
 
 public class StreamsTest {
 	public static void main(String args[]) {
-		Employe e1=new Employe(1,"rajesh",10000,"tpt");
-		Employe e2=new Employe(2,"rakesh",90000,"ptr");
-		Employe e3=new Employe(3,"ramesh",17000,"ctr");
-		Employe e4=new Employe(4,"suresh",40000,"tpt");
+		Employe e1=new Employe(1,"rajesh",100000,"tpt","IT");
+		Employe e2=new Employe(2,"rakesh",90000,"ptr","BPO");
+		Employe e3=new Employe(3,"ramesh",170000,"ctr","IT");
+		Employe e4=new Employe(4,"suresh",40000,"tpt","Teaching");
 		List<Employe> eList=new ArrayList<Employe>();
 		eList.add(e1);
 		eList.add(e2);
 		eList.add(e3);
 		eList.add(e4);
 		eList.stream().forEach(System.out::println);
-		System.out.println("*************************");
+		System.out.println("**************************************************");
 		//sorting employees based on name
 		Comparator <Employe> ecomp=(a,b)->a.getName().compareTo(b.getName());
 		List<Employe> sortedList=eList.stream().sorted(ecomp).collect(Collectors.toList());
 		sortedList.stream().forEach(System.out::println);
-		System.out.println("*************************");
+		System.out.println("*************************************************");
 		eList.stream().collect(Collectors.groupingBy(Employe::getAddress)).
-		forEach((k,v)->System.out.println(" Key :"+k+"value "+v));
+		forEach((k,v)->System.out.println(" Key : "+k+" 	value :"+v));
 		
-		
+				/* group by dept whose sal is greater than 50k
+				 *  
+				 */
+		System.out.println(" Group by dept with sal greater than 50k ");
+		eList.stream().filter(t->t.getSal()>50000).collect(Collectors.groupingBy(Employe::getDept))
+		.forEach((k,v)->System.out.println("Key : "+k+"  Value  :"+v));
 		
 		
 	}
 
 }
  class Employe{
-	 public Employe(int id,String name,long sal,String address ){
+	 public Employe(int id,String name,long sal,String address,String dept ){
 		 this.id=id;
 				 this.name=name;
 		 this.sal=sal;
 		 this.address=address;
+		 this.dept=dept;
+	 }
+	 public void setDept(String dept) {
+		 this.dept=dept;
+	 }
+	 public String getDept() {
+		 return dept;
 	 }
 	 public int getId() {
 		return id;
@@ -67,8 +79,9 @@ public class StreamsTest {
 	 private String name;
 	 private String address;
 	 private long sal;
+	 private String dept;
 	 public String toString() {
-		 return "id : "+id+" name :"+name+" address : "+address+"  sal  : "+sal;
+		 return "id : "+id+" name :"+name+" address : "+address+"  sal  : "+sal+ "Dept "+dept ;
 	 }
 	 
  }
