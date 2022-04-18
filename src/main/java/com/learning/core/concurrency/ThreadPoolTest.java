cefe3f4c6f3fe0921e039bfcb0111031804a2fb9
Rajesh Thokala <rajeshthokala10@gmail.com>
2022-04-18 21:12:24 +0530

    threadpool executor

---
+++ src/main/java/com/learning/core/concurrency/ThreadPoolTest.java
@@ -0,0 +1,45 @@
+package com.learning.core.concurrency;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+public class ThreadPoolTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+        
+        for (int i = 1; i <= 5; i++) 
+        {
+            Task task = new Task("Task " + i);
+            System.out.println("Created : " + task.getName());
+ 
+            executor.execute(task);
+        }
+        executor.shutdown();
+	}
+
+}
+
+
+ class Task implements Runnable {
+    private String name;
+ 
+    public Task(String name) {
+        this.name = name;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void run() {
+        try {
+            Long duration = (long) (Math.random() * 10);
+            System.out.println("Executing : " + name);
+            TimeUnit.SECONDS.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
