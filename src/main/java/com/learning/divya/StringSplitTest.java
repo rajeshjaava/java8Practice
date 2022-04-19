8799fb936d31989f59d9dd51a070b5cf917cb005
Rajesh Thokala <rajeshthokala10@gmail.com>
2022-04-19 22:37:47 +0530

    split

--- src/main/java/com/learning/divya/StringSplitTest.java
+++ src/main/java/com/learning/divya/StringSplitTest.java
@@ -1,23 +1,31 @@
 package com.learning.divya;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StringSplitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s="10 of 100 records";
+		String s="10 of 100 rec 4";
 		String[] s1=s.split("of");
-		s1.toString().lastIndexOf(s);
+		int n = s.lastIndexOf("of");
+		
+		//s1
+		System.out.println(n);
 		List<String> l = new ArrayList<String>();
 		for(String e1: s1 )
 		{
 			System.out.println(e1);
 		}
+		String k1 = "apple orange banana kiwi";
+		String[] k2 =k1.split(" ");
+		//List<String> l1 = new ArrayList<>(Arrays.asList(k2));
+	Arrays.sort(k2);
+	
+	System.out.println("sorted order is " + Arrays.toString(k2));
 		
-
-
 	}
 
 }
