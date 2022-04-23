2304410bb3b6c516110dc40fedfb084c8279b46f
Rajesh Thokala <rajeshthokala10@gmail.com>
2022-04-23 12:31:01 +0530

    added custom funcational interfaces

--- src/main/java/com/learnings/core/CustomFunctionalInterFace.java
+++ src/main/java/com/learnings/core/CustomFunctionalInterFace.java
@@ -1,4 +1,6 @@
 package com.learnings.core;
+
+
 /*
  *  author : Rajesh Thokala
  *  
@@ -7,16 +9,29 @@ public class CustomFunctionalInterFace {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	TestFunction fn= t-> t+"";
+    System.out.println("Function data "+fn.invoke(10));
+     //supplier custom interface
+    SupplierFunction values=()->10;
+    System.out.println("supplier data "+ values.supply());
+    COnsumerFunctionInterface consumeObj=s->System.out.println("Invoked .."+s);
+    consumeObj.consume("Rajesh");
 	}
 
 }
+@FunctionalInterface
  interface TestFunction{
-	 static void test() {
-		
-	}
-	 static void test1() {
-			
-		}
-	 
+	public abstract String invoke(Integer i);
+ }
+
+@FunctionalInterface
+interface SupplierFunction{
+	public abstract Integer supply();
+	
+}
+
+@FunctionalInterface
+interface COnsumerFunctionInterface{
+	public abstract void consume(String s);
+	
 }
