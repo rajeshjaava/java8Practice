4f05b62b88beef53125db291fbed519395aaec65
Rajesh Thokala <rajeshthokala10@gmail.com>
2022-04-25 19:49:39 +0530

    boxed streams

--- src/main/java/com/learnings/core/BoxedStreams.java
+++ src/main/java/com/learnings/core/BoxedStreams.java
@@ -13,8 +13,10 @@ public class BoxedStreams {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Integer> instream=IntStream.of(1,2,3,4,5).boxed().collect(Collectors.toList());
+		List<Integer> instream1=IntStream.of(1,2,3,4,5,4,5).boxed().distinct().collect(Collectors.toList());
 	//	intstream.
 		instream.forEach(System.out::println);
+		instream1.forEach(System.out::println);
 	}
 
 }
