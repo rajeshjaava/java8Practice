59fced9ca0e3ac23880113e5cbcea22278461c13
Rajesh Thokala <rajeshthokala10@gmail.com>
2022-04-25 19:46:23 +0530

    boxed streams

---
+++ src/main/java/com/learnings/core/BoxedStreams.java
@@ -0,0 +1,17 @@
+package com.learnings.core;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/*
+ * author : Rajesh Thokala
+ */
+public class BoxedStreams {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stream<Integer> instream=IntStream.of(1,2,3,4,5).boxed();
+	//	intstream.
+	}
+
+}
