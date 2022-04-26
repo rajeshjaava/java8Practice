4abe5d59e9b2696ab0ef773ae7fea4d5f9ea5661
Rajesh Thokala <rajeshthokala10@gmail.com>
2022-04-26 11:41:06 +0530

    substrings

--- src/main/java/com/learning/divya/SubstringTest.java
+++ src/main/java/com/learning/divya/SubstringTest.java
@@ -4,8 +4,8 @@ public class SubstringTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s="divyasree";
-	System.out.println("subscring  is :  "+s.substring(s.length()-4,s.length()-1));
+	String s="divyasree";
+	System.out.println("subscring  is :  "+s.substring(s.length()-4,s.length()));
 	}
 
 }
