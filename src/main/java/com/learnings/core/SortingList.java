6ebeb5a9a1f2f16cd115ae8c98d91e210596f5d1
Rajesh Thokala <rajeshthokala10@gmail.com>
2022-05-06 10:50:20 +0530

    modified

--- src/main/java/com/learnings/core/SortingList.java
+++ src/main/java/com/learnings/core/SortingList.java
@@ -5,6 +5,7 @@ package com.learnings.core;
  */
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,7 +24,6 @@ public class SortingList {
 		
 		System.out.println("sorting names ");
 		arrayList.stream().sorted(Comparator.comparing(User::getName)).forEach(System.out::println);
-		
 		
 	}
 
