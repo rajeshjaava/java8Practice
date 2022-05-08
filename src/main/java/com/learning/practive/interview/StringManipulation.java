b1ec469b71597e482f23c827de3a7785fc0b0258
Rajesh Thokala <rajeshthokala10@gmail.com>
2022-05-08 12:09:43 +0530

    oracle interview question

--- src/main/java/com/learning/practive/interview/StringManipulation.java
+++ src/main/java/com/learning/practive/interview/StringManipulation.java
@@ -4,7 +4,28 @@ public class StringManipulation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String s="abcdef";
+		int  minCount=manipulate(s);
+	}
+	public static int  manipulate(String s) {
+		int count=0;
+		char[] carray=s.toCharArray();
+		for(Character c:carray) {
+			
+		}
+		for (int i=0;i<s.length()-1/2;i++) {
+			
+			while(s.charAt(i+1)!=s.charAt(i)) {
+				count++;
+				//s.charAt(i+1)=
+				System.out.println(count);
+			}
+			while(s.charAt(s.length()-1-i)!=s.length()-1) {
+				count++;
+				System.out.println(count);
+			}
+		}
+		return count;
 	}
 
 }
