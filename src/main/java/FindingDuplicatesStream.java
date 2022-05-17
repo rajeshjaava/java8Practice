2f8b95308b0c6b5608292f3ea28361e56634d29d
Rajesh Thokala <rajeshthokala10@gmail.com>
2022-05-17 16:11:18 +0530

    added duplicated elements with stream

---
+++ src/main/java/FindingDuplicatesStream.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+/*
+ * author : rajesh thokala
+ */
+public class FindingDuplicatesStream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//10,15,8,49,25,98,98,32,15
+		// duplicate values in list 
+		List<Integer> l=Arrays.asList(10,15,8,49,25,98,98,32,15);
+		//l.stream().map(null);
+		Set<Integer> results =l.stream().filter(x->Collections.frequency(l, x)>1).collect(Collectors.toSet());
+		results.forEach(System.out::println);
+		//l.stream().
+	}
+
+}
--- src/main/java/com/learning/practive/interview/BenzInterview.java
+++ src/main/java/com/learning/practive/interview/BenzInterview.java
@@ -1,5 +1,7 @@
 package com.learning.practive.interview;
-
+/*
+ * take the string and 
+ */
 public class BenzInterview {
 	public static void main(String args[]) {
 		
